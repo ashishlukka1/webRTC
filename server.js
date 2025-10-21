cc98f2c3e2b4971e2923cd3452855a9f7dba0e7e
Ashish Lukka <ashishlukka2005@gmail.com>
2025-10-21 21:04:13 +0530

    v1

---
+++ server.js
@@ -0,0 +1,357 @@
+"use strict";
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var WebSocketServer = require('websocket').server;
+
+var url = require('url');
+
+// Used for managing the text chat user list.
+
+var connectionArray = [];
+var nextID = Date.now();
+var appendToMakeUnique = 1;
+
+// Output logging information to console
+
+function log(text) {
+  var time = new Date();
+
+  console.log("[" + time.toLocaleTimeString() + "] " + text);
+}
+
+
+function originIsAllowed(origin) {
+  return true;    // We will accept all connections
+}
+
+// Scans the list of users and see if the specified name is unique. If it is,
+// return true. Otherwise, returns false. We want all users to have unique
+// names.
+function isUsernameUnique(name) {
+  var isUnique = true;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].username === name) {
+      isUnique = false;
+      break;
+    }
+  }
+  return isUnique;
+}
+
+// Sends a message (which is already stringified JSON) to a single
+// user, given their username. We use this for the WebRTC signaling,
+// and we could use it for private text messaging.
+function sendToOneUser(target, msgString) {
+  var isUnique = true;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].username === target) {
+      connectionArray[i].sendUTF(msgString);
+      break;
+    }
+  }
+}
+
+// Scan the list of connections and return the one for the specified
+// clientID. Each login gets an ID that doesn't change during the session,
+// so it can be tracked across username changes.
+function getConnectionForID(id) {
+  var connect = null;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].clientID === id) {
+      connect = connectionArray[i];
+      break;
+    }
+  }
+
+  return connect;
+}
+
+// Builds a message object of type "userlist" which contains the names of
+// all connected users. Used to ramp up newly logged-in users and,
+// inefficiently, to handle name change notifications.
+function makeUserListMessage() {
+  var userListMsg = {
+    type: "userlist",
+    users: []
+  };
+  var i;
+
+  // Add the users to the list
+
+  for (i=0; i<connectionArray.length; i++) {
+    userListMsg.users.push(connectionArray[i].username);
+  }
+
+  return userListMsg;
+}
+
+// Sends a "userlist" message to all chat members. This is a cheesy way
+// to ensure that every join/drop is reflected everywhere. It would be more
+// efficient to send simple join/drop messages to each user, but this is
+// good enough for this simple example.
+function sendUserListToAll() {
+  var userListMsg = makeUserListMessage();
+  var userListMsgStr = JSON.stringify(userListMsg);
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    connectionArray[i].sendUTF(userListMsgStr);
+  }
+}
+
+// Create HTTP server
+var httpServer = http.createServer(function(request, response) {
+  // Log all incoming requests
+  log("Incoming request: " + request.method + " " + request.url);
+
+  // Handle health check
+  if (request.url === '/health') {
+    response.writeHead(200);
+    response.end('OK');
+    return;
+  }
+
+  // Handle favicon.ico request
+  if (request.url === '/favicon.ico') {
+    response.writeHead(204); // No Content
+    response.end();
+    return;
+  }
+
+  // Handle WebSocket upgrade requests
+  if (request.url === '/ws') {
+    response.writeHead(200);
+    response.end();
+    return;
+  }
+let parsedUrl = url.parse(request.url);
+let filePath = path.join(__dirname, parsedUrl.pathname);
+if (filePath === path.join(__dirname, '/')) {
+  filePath = path.join(__dirname, 'index.html');
+}
+
+  // Handle static file requests
+  // let filePath = path.join(__dirname, request.url);
+  // if (filePath === path.join(__dirname, '/')) {
+  //   filePath = path.join(__dirname, 'index.html');
+  // }
+
+  // Log the file path being requested
+  log("Attempting to serve file: " + filePath);
+
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+  
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+    case '.ico':
+      contentType = 'image/x-icon';
+      break;
+  }
+
+  fs.readFile(filePath, function(error, content) {
+    if (error) {
+      if(error.code === 'ENOENT') {
+        log("File not found: " + filePath);
+        response.writeHead(404);
+        response.end('File not found: ' + filePath);
+      } else {
+        log("Server error: " + error.code + " for file: " + filePath);
+        response.writeHead(500);
+        response.end('Server Error: ' + error.code);
+      }
+    } else {
+      log("Successfully serving file: " + filePath);
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
+    }
+  });
+});
+
+// Get port from environment variable
+const PORT = process.env.PORT || 10000;
+
+// Spin up the HTTP server
+httpServer.listen(PORT, '0.0.0.0', function() {
+  log("Server is listening on port " + PORT);
+  log("Current working directory: " + process.cwd());
+  log("Server directory: " + __dirname);
+  log("Directory contents:");
+  fs.readdir(__dirname, (err, files) => {
+    if (err) {
+      log("Error reading directory: " + err);
+    } else {
+      files.forEach(file => {
+        log(" - " + file);
+      });
+    }
+  });
+});
+
+// Create the WebSocket server by converting the HTTP server into one
+var wsServer = new WebSocketServer({
+  httpServer: httpServer,
+  autoAcceptConnections: false
+});
+
+if (!wsServer) {
+  log("ERROR: Unable to create WbeSocket server!");
+}
+
+// Set up a "connect" message handler on our WebSocket server. This is
+// called whenever a user connects to the server's port using the
+// WebSocket protocol.
+
+wsServer.on('request', function(request) {
+  if (!originIsAllowed(request.origin)) {
+    request.reject();
+    log("Connection from " + request.origin + " rejected.");
+    return;
+  }
+
+  // Accept the request and get a connection.
+
+  var connection = request.accept("json", request.origin);
+
+  // Add the new connection to our list of connections.
+
+  log("Connection accepted from " + connection.remoteAddress + ".");
+  connectionArray.push(connection);
+
+  connection.clientID = nextID;
+  nextID++;
+
+  // Send the new client its token; it send back a "username" message to
+  // tell us what username they want to use.
+
+  var msg = {
+    type: "id",
+    id: connection.clientID
+  };
+  connection.sendUTF(JSON.stringify(msg));
+
+  // Set up a handler for the "message" event received over WebSocket. This
+  // is a message sent by a client, and may be text to share with other
+  // users, a private message (text or signaling) for one user, or a command
+  // to the server.
+
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      log("Received Message: " + message.utf8Data);
+
+      // Process incoming data.
+
+      var sendToClients = true;
+      msg = JSON.parse(message.utf8Data);
+      var connect = getConnectionForID(msg.id);
+
+      // Take a look at the incoming object and act on it based
+      // on its type. Unknown message types are passed through,
+      // since they may be used to implement client-side features.
+      // Messages with a "target" property are sent only to a user
+      // by that name.
+
+      switch(msg.type) {
+        // Username change
+        case "username":
+          var nameChanged = false;
+          var origName = msg.name;
+
+          // Ensure the name is unique by appending a number to it
+          // if it's not; keep trying that until it works.
+          while (!isUsernameUnique(msg.name)) {
+            msg.name = origName + appendToMakeUnique;
+            appendToMakeUnique++;
+            nameChanged = true;
+          }
+
+          // If the name had to be changed, we send a "rejectusername"
+          // message back to the user so they know their name has been
+          // altered by the server.
+          if (nameChanged) {
+            var changeMsg = {
+              id: msg.id,
+              type: "rejectusername",
+              name: msg.name
+            };
+            connect.sendUTF(JSON.stringify(changeMsg));
+          }
+
+          // Set this connection's final username and send out the
+          // updated user list to all users. Yeah, we're sending a full
+          // list instead of just updating. It's horribly inefficient
+          // but this is a demo. Don't do this in a real app.
+          connect.username = msg.name;
+          sendUserListToAll();
+          sendToClients = false;  // We already sent the proper responses
+          break;
+      }
+
+      // Convert the revised message back to JSON and send it out
+      // to the specified client or all clients, as appropriate. We
+      // pass through any messages not specifically handled
+      // in the select block above. This allows the clients to
+      // exchange signaling and other control objects unimpeded.
+
+      if (sendToClients) {
+        var msgString = JSON.stringify(msg);
+        var i;
+
+        // If the message specifies a target username, only send the
+        // message to them. Otherwise, send it to every user.
+        if (msg.target && msg.target !== undefined && msg.target.length !== 0) {
+          sendToOneUser(msg.target, msgString);
+        } else {
+          for (i=0; i<connectionArray.length; i++) {
+            connectionArray[i].sendUTF(msgString);
+          }
+        }
+      }
+    }
+  });
+
+  // Handle the WebSocket "close" event; this means a user has logged off
+  // or has been disconnected.
+  connection.on('close', function(reason, description) {
+    // First, remove the connection from the list of connections.
+    connectionArray = connectionArray.filter(function(el, idx, ar) {
+      return el.connected;
+    });
+
+    // Now send the updated user list. Again, please don't do this in a
+    // real application. Your users won't like you very much.
+    sendUserListToAll();
+
+    // Build and output log output for close information.
+
+    var logMessage = "Connection closed: " + connection.remoteAddress + " (" +
+                     reason;
+    if (description !== null && description.length !== 0) {
+      logMessage += ": " + description;
+    }
+    logMessage += ")";
+    log(logMessage);
+  });
+});
